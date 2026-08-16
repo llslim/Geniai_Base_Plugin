87b127f7f2d954f18a6f43dea3237fa5ff141385
AAC Developer <dev@llslim.com>
2026-08-16 15:59:00 -0400

    chore: bump version to 2026081005 / 2.4.5 for AI scenario builder

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026081004;
+$plugin->version = 2026081005;
 $plugin->requires = 2025041400;
-$plugin->release = "2.4.4";
+$plugin->release = "2.4.5";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "local_aacuracore";
